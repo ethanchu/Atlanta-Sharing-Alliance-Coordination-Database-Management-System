84729eccf222327da8f9ed1ed1f9059cae4a2bbc
Priyanka Gupta <Priyanka@priyankas-air.attlocal.net>
2017-04-12 17:31:26 -0700

    Foodbank main menu

---
+++ Phase3_v1/FBmainmenu.php
@@ -0,0 +1,42 @@
+
+
+<html>
+
+<head>
+
+<title>Foodbank Main Menu </title>
+<link rel="stylesheet" type="text/css" href="site.css">
+</head>
+
+
+<body>
+
+<a href="siteservice.php"><button type="submit">Go Back to Site Service</button></a>
+<!--<h1>Back To Home </h1> -->
+<br>
+<br>
+<br>
+
+<!-- For Add item page -->
+<div class="container">
+<a href="add_item.php"><button type="submit">Add Items</button></a>
+</div>
+
+
+<!-- For edit item page -->
+<div class="container">
+<a href="edit_item.php"><button type="submit">Edit Items</button></a>
+</div>
+
+
+<!-- For Request Status Report page -->
+<div class="container">
+<a href="view_request.php"><button type="submit">Request Status Report</button></a>
+</div>
+
+
+
+
+</body>
+
+</html>
